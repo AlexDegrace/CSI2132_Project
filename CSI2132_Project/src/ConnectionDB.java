a257207a653b1ecc3d114904adb74662afcf3e93
Thomas Ouellette <43257395+touel068@users.noreply.github.com>
2021-03-31 15:40:10 -0400

    ConnectionDB insert and delete

--- CSI2132_Project/src/ConnectionDB.java
+++ CSI2132_Project/src/ConnectionDB.java
@@ -201,5 +201,174 @@ public class ConnectionDB {
 	}
 	
 	
+	//insert into customers
+	public boolean insertCustomers(String customer_id, String first_name, String middle_name, String last_name, String street_number, String street_name, String city, String state_province, String country, String postal_code, String sin, String date_of_birth, String phone) {
+		getConnection();
+		
+		boolean worked = false;
+		
+		try {
+			String query = "INSERT INTO public.customer(customer_id,first_name,middle_name,last_name,street_number,street_name,city,state_province,country,postal_code,sin,date_of_birth,phone) VALUES ("+customer_id + ", '" + first_name + "' , '" + middle_name + "' , '" + last_name + "' , " + street_number + ", '" + street_name + "' , '" + city + "' , '" + state_province + "' , '" + country + "' , '" + postal_code + "' , " + sin + " , '" + date_of_birth + "' , " + phone + ");";
+		st = db.createStatement();
+		st.execute(query);
+		worked = true;
+		}catch(SQLException e) {
+			e.printStackTrace();
+			worked=false;
+		}finally {
+			closeDB();
+		}
+		return worked;
+		
+	}
+	
+	//insert into staff
+	public boolean insertStaff(String staff_id, String first_name, String middle_name, String last_name, String street_number, String street_name, String city, String state_province, String country, String postal_code, String sin, String salary, String employee_job, String hotel_id) {
+		getConnection();
+		
+		boolean worked = false;
+		
+		try {
+			String salaryNoDollarsSign = salary.substring(1,salary.length()-1);
+			String query = "INSERT INTO public.staff(staff_id,first_name,middle_name,last_name,street_number,street_name,city,state_province,country,postal_code,sin,salary,employee_job,hotel_id) VALUES ("+staff_id + ", '" + first_name + "' , '" + middle_name + "' , '" + last_name + "' , " + street_number + ", '" + street_name + "' , '" + city + "' , '" + state_province + "' , '" + country + "' , '" + postal_code + "' , " + sin + " , " + salaryNoDollarsSign + " , '"+employee_job +" , " + hotel_id + ");";
+		st = db.createStatement();
+		st.execute(query);
+		worked = true;
+		}catch(SQLException e) {
+			e.printStackTrace();
+			worked=false;
+		}finally {
+			closeDB();
+		}
+		return worked;
+		
+	}
+	
+	//insert into hotel_chain
+		public boolean insertHotel(String hotel_id, String brand_name, String star_category, String street_number, String street_name, String city, String state_province, String country, String postal_code,  String contact_email, String phone_number, String manager_id) {
+			getConnection();
+			
+			boolean worked = false;
+			
+			try {
+				String query = "INSERT INTO public.hotel_chain(hotel_id,brand_name,star_category,street_number,street_name,city,state_province,country,postal_code,contact_email,phone_number,manager_id) VALUES ("+hotel_id + ", '" + brand_name + "' , " + star_category + " , " + street_number + ", '" + street_name + "' , '" + city + "' , '" + state_province + "' , '" + country + "' , '" + postal_code + "' , '" + contact_email + "' , '" + phone_number + "' , " + manager_id + ");";
+			st = db.createStatement();
+			st.execute(query);
+			worked = true;
+			}catch(SQLException e) {
+				e.printStackTrace();
+				worked=false;
+			}finally {
+				closeDB();
+			}
+			return worked;
+			
+		}
+	
+		
+		//insert into room
+			public boolean insertRoom(String room_id, String hotel_id, String price, String room_capacity, String is_extendable) {
+					getConnection();
+					
+					boolean worked = false;
+					
+					try {
+						String priceNoDollarsSign = price.substring(1,price.length()-1);
+						String query = "INSERT INTO public.room(room_id,hotel_id,price,room_capacity,is_extendable) VALUES ("+room_id + ","+hotel_id+ ","+priceNoDollarsSign + ","+ room_capacity + "," + is_extendable + ");";
+					st = db.createStatement();
+					st.execute(query);
+					worked = true;
+					}catch(SQLException e) {
+						e.printStackTrace();
+						worked=false;
+					}finally {
+						closeDB();
+					}
+					return worked;
+					
+				}
+			
+			public boolean deleteCustomer(String customer_id) {
+				getConnection();
+				
+				boolean worked = false;
+				
+				try {
+					String query = "DELETE FROM public.customer WHERE customer_id =" + customer_id;
+				st = db.createStatement();
+				st.execute(query);
+				worked = true;
+				}catch(SQLException e) {
+					e.printStackTrace();
+					worked=false;
+				}finally {
+					closeDB();
+				}
+				return worked;
+				
+			}
+			
+			public boolean deleteStaff(String staff_id) {
+				getConnection();
+				
+				boolean worked = false;
+				
+				try {
+					String query = "DELETE FROM public.staff WHERE staff_id =" + staff_id;
+				st = db.createStatement();
+				st.execute(query);
+				worked = true;
+				}catch(SQLException e) {
+					e.printStackTrace();
+					worked=false;
+				}finally {
+					closeDB();
+				}
+				return worked;
+				
+			}
+			
+			public boolean deleteHotel(String hotel_id) {
+				getConnection();
+				
+				boolean worked = false;
+				
+				try {
+					String query = "DELETE FROM public.hotel_chain WHERE hotel_id =" + hotel_id;
+				st = db.createStatement();
+				st.execute(query);
+				worked = true;
+				}catch(SQLException e) {
+					e.printStackTrace();
+					worked=false;
+				}finally {
+					closeDB();
+				}
+				return worked;
+				
+			}
+			
+			public boolean deleteRoom(String room_id) {
+				getConnection();
+				
+				boolean worked = false;
+				
+				try {
+					String query = "DELETE FROM public.room_id WHERE room_id =" + room_id;
+				st = db.createStatement();
+				st.execute(query);
+				worked = true;
+				}catch(SQLException e) {
+					e.printStackTrace();
+					worked=false;
+				}finally {
+					closeDB();
+				}
+				return worked;
+				
+			}
+			
+	
+	
 
 }
